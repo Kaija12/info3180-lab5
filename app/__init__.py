2677a62da9b18ef05a73c1811944281de7903124
Kaija Hall <kaija1342@gmail.com>
2026-04-13 19:36:57 -0500

    Added movies database table

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 from app import views
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kaija@localhost/info3180-lab5'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
