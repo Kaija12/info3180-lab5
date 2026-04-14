9e3df86e3da072b90bdd1969f8f9549d10011942
Kaija Hall <kaija1342@gmail.com>
2026-04-13 19:37:56 -0500

    Edited movies database table

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,6 @@ app.config.from_object(Config)
 
 from app import views
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kaija@localhost/info3180-lab5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/info3180-lab5'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
